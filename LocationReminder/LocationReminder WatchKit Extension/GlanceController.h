5c9fb9ba0140b661755813b915d934ee65dfd592
John Leonard <cwnidog1@gmail.com>
2015-02-05 15:46:41 -0800

    Adding WatchKit functionality to display reminder on AppleWatch.

---
+++ LocationReminder/LocationReminder WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  LocationReminder WatchKit Extension
+//
+//  Created by John Leonard on 2/5/15.
+//  Copyright (c) 2015 John Leonard. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
